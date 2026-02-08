cd9b09e195827335d79f619812eeb1bfa4874a43
Sultan Ansari <ansarisultan443@gmail.com>
2026-02-08 19:40:56 +0530

    adding new commit

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
-print("Hello dosto, I am learning git")
+name="sultan"
+My_country = "India"
+Age = 29
+print("My name is", name, "I am", Age, "years old")
+print("His name is " + name)
+print(f"I am from {My_country} and going to turn {Age} old")
+age = input("Enter your birth year")
+print("your birth year is", age)
